c7278c853b0e353c79dd63660f19c51ab60a1b94
unknown <nila@AD.THAMESSOFTWARESOLUTIONS.CO.UK>
2026-06-18 15:34:45 +0100

    Jpa repo

--- demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/repository/EmployeeJpaRepository.java
+++ demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/repository/EmployeeJpaRepository.java
@@ -4,6 +4,12 @@ import com.example.demoDataJpa.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeJpaRepository extends CrudRepository<Employee, Integer> {
+
+    Optional<Employee> findByIdAndName(Integer id, String name);
+
+    Optional<Employee> findByName(String name);
 }
--- demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/service/EmployeeService.java
+++ demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/service/EmployeeService.java
@@ -5,10 +5,15 @@ import com.example.demoDataJpa.repository.EmployeeJpaRepository;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @EnableTransactionManagement
@@ -23,33 +28,47 @@ public class EmployeeService {
 
 
     public Employee getEmployeeFromDB(Integer id){
-
+        Optional<Employee> oe =  employeeJpaRepository.findById(id);
+        if(oe.isPresent()) {
+            return  oe.get();
+        }
+        throw new ResponseStatusException(HttpStatusCode.valueOf(404), "Employee not found");
     }
 
     public Employee getEmployeeByName(String name){
-
+        Optional<Employee> oe =  employeeJpaRepository.findByName(name);
+        if(oe.isPresent()) {
+            return  oe.get();
+        }
+        throw new ResponseStatusException(HttpStatusCode.valueOf(404), "Employee not found");
 
     }
 
     @Transactional
     public void updateEmpInDb(Integer id, Employee employee) {
-
-
+        Employee orig = getEmployeeFromDB(id);
+        orig.setSalary(employee.getSalary());
+        employeeJpaRepository.save(orig);
     }
 
     @Transactional
     public void deleteEmpInDb(Integer id) {
-
+        employeeJpaRepository.deleteById(id);
     }
 
     @Transactional
     public Employee createEmpInDb(Employee employee) {
-
+        return employeeJpaRepository.save(employee);
     }
 
     public List<Employee> getAllEmpFromDb() {
 
-
+        Iterator<Employee> employeeIterator = employeeJpaRepository.findAll().iterator();
+        List<Employee> employeeList = new ArrayList<>();
+        while (employeeIterator.hasNext()) {
+            employeeList.add(employeeIterator.next());
+        }
+        return employeeList;
     }
 
 }
