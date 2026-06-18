7e3be8826ad2f3d4295200d7a9d5b9c1a22f883b
unknown <nila@AD.THAMESSOFTWARESOLUTIONS.CO.UK>
2026-06-18 15:44:21 +0100

    feature flag

--- demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/controller/EmployeeController.java
+++ demoDataJpa/demoDataJpa/src/main/java/com/example/demoDataJpa/controller/EmployeeController.java
@@ -2,8 +2,10 @@ package com.example.demoDataJpa.controller;
 
 import com.example.demoDataJpa.model.Employee;
 import com.example.demoDataJpa.repository.EmployeeRepository;
+import com.example.demoDataJpa.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,16 +16,24 @@ import java.util.List;
 @RequestMapping(path = "/employee", produces = {"application/json"})
 public class EmployeeController {
 
+    @Value("${enabled.service}")
+    private String jpaEnabled;
+
     private EmployeeRepository repository;
 
+    private EmployeeService service;
+
     @Autowired
-    public EmployeeController(EmployeeRepository repository) {
+    public EmployeeController(EmployeeRepository repository, EmployeeService service) {
         this.repository = repository;
+        this.service = service;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public Employee getEmployee(@RequestParam Integer id) {
-
+        if(jpaEnabled.equals("jpa")) {
+            return service.getEmployeeFromDB(id);
+        }
         return  repository.getEmployeeFromDB(id);
     }
 
